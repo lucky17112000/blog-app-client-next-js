c50365e47dfa3128fc0fd22edf098dc60ceff373
Alamin Mustafa Rahim <lucky17112000@gmail.com>
2026-02-07 10:53:22 +0600

    fetch post data from backend(basic)

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
+import { blogService } from "@/services/blog.services";
 import { userService } from "@/services/user.services";
 import { cookies } from "next/headers";
 
@@ -8,7 +9,8 @@ export default async function Home() {
 
 //  const {data , error} =await userService.getSession();
 //  console.log("Session data in page.tsx:", data);
-
+const {data} = await blogService.getBlogPosts();
+console.log("Blog posts data in page.tsx:", data);
   return(
     <div>
       <Button>Click here</Button>
